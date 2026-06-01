5254356a372fc054b8fd6139eaedb46e5e577673
afrandormd <afrandormd@gmail.com>
2026-06-01 19:09:24 +0700

    feat: update spinner & moviecard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
 import Search from "./components/Search"
+import Spinner from "./components/Spinner"
+import MovieCard from "./components/MovieCard"
 
 const API_BASE_URL = 'https://api.themoviedb.org/3'
 
@@ -17,11 +19,13 @@ const App = () => {
     const [searchTerm, setSearchTerm] = useState('')
     const [errorMessage, setErrorMessage] = useState('')
     const [movieList, setMovieList] = useState([])
-    const [loading, setIsLoading] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
 
 
     const fetchMovies = async () => {
         try {
+            setIsLoading(true)
+
             const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
 
             const response = await fetch(endpoint, API_OPTIONS)
@@ -43,6 +47,8 @@ const App = () => {
         } catch (error) {
             console.error(`Error fetching movies: ${error}`)
             setErrorMessage('Error fetching movies. Please try again later.')
+        } finally {
+            setIsLoading(false)
         }
     }
 
@@ -63,8 +69,18 @@ const App = () => {
                 </header>
 
                 <section className="all-movies">
-                    <h2>All Movies</h2>
-                    {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+                    <h2 className="mt-[40px]">All Movies</h2>
+                    {isLoading ? (
+                        <Spinner />
+                    ) : errorMessage ? (
+                        <p className="text-red-500">{errorMessage}</p>
+                    ) : (
+                        <ul>
+                            {movieList.map((movie) => (
+                                <MovieCard key={movie.id} movie={movie} />
+                            ))}
+                        </ul>
+                    )}
                 </section>
 
             </div>
